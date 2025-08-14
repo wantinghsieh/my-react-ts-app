d1cbd85296ac418db705f6fc9b14c88b33b12477
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-14 15:30:26 -0400

    More Reusable Components

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,11 +1,18 @@
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
 
 interface ProjectListProps {
   projects: Project[];
 }
 
-function ProjectList({ projects }: ProjectListProps) {
-  return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+function ProjectList ({ projects }: ProjectListProps) {
+    const items = projects.map(project => (
+      <div key={project.id} className="cols-sm">
+      <ProjectCard project={project} />
+      </div>
+    ));
+    return <div className="row">{items}</div>;
+
 }
 
 export default ProjectList;
