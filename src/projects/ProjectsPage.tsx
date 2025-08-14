7b7229fad5595fd7511a6185a09b1bfd126c41cd
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-14 14:22:14 -0400

    add Data

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,5 +1,12 @@
+import { MOCK_PROJECTS } from './MockProjects';
+
 function ProjectsPage() {
-    return <h1>Projects</h1>;
-  }
-  
-  export default ProjectsPage;
+  return (
+   <>
+     <h1>Projects</h1>
+    <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+   </>
+  );
+}
+
+export default ProjectsPage;
