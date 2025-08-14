e4311e98bd9759347da12a766e5ac6b7ceec484f
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-14 15:10:41 -0400

    Passing Data to a Component

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,11 +1,12 @@
 import { MOCK_PROJECTS } from './MockProjects';
+import ProjectList from './ProjectList';
 
 function ProjectsPage() {
   return (
-   <>
-     <h1>Projects</h1>
-    <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
-   </>
+    <>
+      <h1>Projects</h1>
+     <ProjectList projects={MOCK_PROJECTS} />
+    </>
   );
 }
 
