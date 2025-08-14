5dfa7d809692e78df48c1a736c73b258142727d1
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-14 14:02:21 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+   return (
+   <div className="container">
+      <ProjectsPage />
+   </div>
+   );
+}
+export default App;
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,5 @@
+function ProjectsPage() {
+    return <h1>Projects</h1>;
+  }
+  
+  export default ProjectsPage;
